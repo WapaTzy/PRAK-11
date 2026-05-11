7f4311c38d7a88697269cf504b4dbeb052366c9e
“WapaTzy” <wafarozanu22@gmail.com>
2026-05-11 18:24:28 +0700

    Menambahkan metode perhitungan jumlah pada base class orang

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class dokter;
-class pasien { // asosiasi
+class pasien { 
 public:
     string nama;
     vector<dokter*> daftar_dokter;
@@ -39,8 +39,7 @@ void pasien::tambahDokter(dokter* pDokter) {
 
 void pasien::cetakDokter() {
     cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
-    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan untuk secara otomatis menentukan tipe data dari elemen yang diiterasi
-        cout << a->nama << "\n";
+    for (auto& a : daftar_dokter) { 
     }
     cout << endl;
 }
